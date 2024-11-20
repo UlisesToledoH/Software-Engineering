468da18ac3b6582feb0a5b9e9c9520aadda5ed54
Ulises Toledo <ugth13@gmail.com>
2024-11-20 11:13:19 -0600

    nuevoarchivo

---
+++ Funciones.go
@@ -0,0 +1,16 @@
+// package main
+
+// import "fmt"
+
+// func suma(a int, b int) int {
+// 	return a + b
+// }
+
+// func imprime_resultado(a int, b int) {
+// 	fmt.Println("la suma de ", a, " y ", b, " es ", suma(a, b))
+// }
+
+// func main() {
+// 	imprime_resultado(12, 3)
+
+// }
